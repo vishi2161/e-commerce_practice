cdf0fc041365cfe801288b446a8cb3f65b4317e7
Manikandan27031999 <154354794+Manikandan27031999@users.noreply.github.com>
2025-11-07 00:02:04 +0530

    user entity created

---
+++ userservice/userservice/src/main/java/com/ecommerce/userservice/entity/User.java
@@ -0,0 +1,12 @@
+package com.ecommerce.userservice.entity;
+
+public class User {
+
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+}
